00908937d0c69c88e717d82041e3e2aff4c316de
Vojislav123 <vojislavalic@hotmail.com>
2023-07-07 22:30:49 +0200

    creating a wikipedia search engine
    Testing out dynamic  pages

---
+++ types.d.ts
@@ -0,0 +1,22 @@
+type Result = {
+    pageid: string,
+    title: string,
+    extract: string,
+    thumbnail?: {
+        source: string,
+        width: number,
+        height: number,
+    }
+}
+
+type SearchResult = {
+    query?: {
+        pages?: Result[],
+    },
+}
+
+type Props = {
+    params: {
+        searchTerm: string
+    }
+}
